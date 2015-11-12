2c5ae5f0db1ed61cb0faafb028174d0fada8d982
Balbali <Balbali@172.17.199.154>
2015-11-12 17:30:58 +0100

    j'ai enlevé 2 lignes de code

--- index.php
+++ index.php
@@ -22,8 +22,7 @@ if(isset($_POST['OK'])) {
 <h3>Reservation</h3><br />
 <fieldset><legend>Compte</legend>
     <p>Admin</p>
-    <p>Mail : admin@admin.fr</p>
-    <p>Mot de passe : admin</p>
+    
     <p>Utilisateur</p>
     <p>Mail : test@test.fr</p>
     <p>Mot de passe : test</p>
